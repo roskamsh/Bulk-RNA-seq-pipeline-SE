5d91087e8ec655d9588aca247125b50c73ad2516
roskamsh <roskamsh@exanode-3-7.local>
2019-04-25 22:31:18 -0700

    Added scale_colour_manual to ggplot function

--- scripts/density_plot.R
+++ scripts/density_plot.R
@@ -22,19 +22,25 @@ cat(sprintf(c('Density plot : ', density_plot, '\n')))
 colors <- snakemake@params['colors']
 discrete <- snakemake@params['discrete']
 
+# function to grab the ggplot2 colours
+gg_color_hue <- function(n) {
+  hues = seq(15, 375, length = n + 1)
+  hcl(h = hues, l = 65, c = 100)[1:n]
+}
 
 rld = readRDS(rld)
 normed_values = assay(rld)
 normed_t = t(normed_values)
 meta = colData(rld)
 
-
 if(colors!='NA' & discrete =='NA'){
-    if(brewer.pal.info[colors]$maxcolors >= length(levels(meta[condition]))){
-        pal <- colorRampPalette(brewer.pal(length(levels(meta[condition])),name=colors))
-    }else if(discrete != 'NA'){
+    if(brewer.pal.info[colors]$maxcolors >= length(levels(meta[[condition]]))) {
+        pal <- colorRampPalette(brewer.pal(length(levels(meta[[condition]])),name=colors))
+    } 
+} else if(discrete != 'NA'){
         pal <- as.vector(discrete)
-    }
+} else {
+        pal <- gg_color_hue(length(levels(meta[[condition]])))
 }
 
 joined_counts = cbind(meta[condition],normed_t)
@@ -47,6 +53,8 @@ pdf(density_plot)
 p<-ggplot(mm, aes_string(x='value', color=condition)) +
   geom_density()+
   geom_vline(data=mu, aes_string(xintercept='grp.mean', color=condition),
-             linetype="dashed") + xlab('regularized log expression') + ggtitle(eval(project_id)) + theme(plot.title = element_text(hjust = 0.5))
+             linetype="dashed") + xlab('regularized log expression') + 
+  scale_color_manual(values=pal) +
+  ggtitle(eval(project_id)) + theme(plot.title = element_text(hjust = 0.5))
 p
 dev.off()

1f765215a63052076e6045d9e4bb0220c89307a5
Breeshey Roskams-Hieter <42788995+roskamsh@users.noreply.github.com>
2019-05-01 14:28:29 -0700

    removed unnecessary command-line args

--- scripts/RNAseq_filterCounts.R
+++ scripts/RNAseq_filterCounts.R
@@ -8,54 +8,7 @@ countsFile <- snakemake@input[['countsFile']]
 
 mito <- snakemake@params[['mito']]
 
-
-help <- function(){
-    cat("RNAseq_filterCounts.R :
-- Script to filter counts table based on biotype and/or mito genes.
-- Assumes that the genes are in their own column called Genes and are common names not Ensembl ids.
-- Anno file must have columns: external_gene_name, gene_biotype.
-- You can find biotype discriptions here:
-     https://uswest.ensembl.org/info/genome/genebuild/biotypes.html
-     https://www.gencodegenes.org/pages/biotypes.html
-\n")
-    cat("Usage: \n")
-    cat("--countsFile : counts table to be filtered (.txt tab sep )     [ required ]\n")
-    cat("--annoFile   : FDR adjusted p-value cutoff      [ required ]\n")
-    cat("--mito       : remove mito genes (1/0)          [ default = 1 ]\n")
-    cat("--biotypes   : comma seperated biotypes to keep [ required ]
-           example: --biotypes=protein_coding,lincRNA,sRNA,rRNA,snRNA,snoRNA\n")
-    
-    cat("\n")
-    q()
-}
-
-## Save values of each argument
-if(!is.na(charmatch("--help",args)) || !is.na(charmatch("-h",args))){
-    help()
-}
-
-## set defaults if options are not provided
-if (identical( mito, character(0) )){
-    mito <- 1
-}else{
-    mito <- as.numeric(mito)
-}
-
-if (identical( biotypes, character(0) )){
-    biotypes <- ""
-}
-
-##----------load counts------------#
-print("Loading counts table")
-print(countsFile)
-
-## check if an rda file or tab sep
-if(grepl('rda|RData|Rdata',countsFile)){
-    counts <- get(load(file=countsFile))
-}
-if(grepl('txt|tsv',countsFile)){
-    counts <- read.delim(file=countsFile)
-}
+counts <- read.delim(file=countsFile)
 
 ##----------load counts------------#
 print("Loading counts table")
@@ -69,13 +22,8 @@ counts <- read.delim(file=countsFile)
 print("Loading annotation table")
 print(annoFile)
 
-## check if an rda file or tab sep
-if(grepl('rda|RData|Rdata',annoFile)){
-    anno <- get(load(file=annoFile))
-}
-if(grepl('txt|tsv',annoFile)){
-    anno <- read.delim(file=annoFile)
-}
+## load
+anno <- get(load(file=annoFile))
 
 if(strsplit(biotypes, split='\\,')[[1]]!=""){
     anno.sub <- anno[paste(anno$gene_biotype) %in% strsplit(biotypes, split='\\,')[[1]] ,]
